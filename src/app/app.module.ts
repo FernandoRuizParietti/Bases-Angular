5e3c202a4bd2064b8c4b85baaa8145d1620fc3d0
FernandoRuizParietti <ferdyjrp@yahoo.com.ar>
2022-04-29 18:37:30 -0300

    segundo commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,17 +2,19 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { ContadorComponent } from './contador/contador.component';
-import { HeroeComponent } from './heroes/heroe/heroe.component';
+import { HeroesModule } from './heroes/heroes.module';
+import { ContadorModule } from './contador/contador.module';
+import { DbzModule } from './dbz/dbz.module';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ContadorComponent,
-    HeroeComponent
-  ],
+     ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HeroesModule,
+    ContadorModule,
+    DbzModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/heroes/heroe/heroe.component.ts
+++ src/app/heroes/heroe/heroe.component.ts
@@ -6,5 +6,22 @@ import { Component } from '@angular/core';
 })
 
 export class HeroeComponent{
-     
+     nombre:string = 'Iroman';
+     edad : number = 41;
+
+     get nombreCapitalizado(): string{
+         return this.nombre.toUpperCase();
+     }
+
+     obtenerNombre(): string{
+        return `${this.nombre} - ${this.edad}`;
+     }
+
+     cambiarNombre():void{
+        this.nombre= 'Spiderman';
+     }
+
+     cambiarEdad():void{
+         this.edad = 18;
+     }
 }
